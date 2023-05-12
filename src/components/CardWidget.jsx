ababeccc4bb35959924d30ca101409da19cf157f
Melissa Nicolau <feddeqb.fqb@gmail.com>
2023-05-11 23:59:13 -0300

    PreEntrega1QuirozBaez

---
+++ src/components/CardWidget.jsx
@@ -0,0 +1,10 @@
+import Carrito from "./images/carrito.png"
+
+const CardWidget=()=>{
+    return(
+        <>
+        <a href="#"><img src={Carrito} width={40}/></a>
+        </>
+    )
+}
+export default CardWidget;
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,6 @@
+const ItemListContainer=()=>{
+    return(
+        <h1>ItemListContainer</h1>
+    )
+}
+export default ItemListContainer;
